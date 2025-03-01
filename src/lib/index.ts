a545396ac758f04a5222312b3b08a6c09628ab96
Lakshya0304 <lakshyakhandelwal460@gmail.com>
2025-03-02 04:14:02 +0530

    change database

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient(
-    // {
-    //     datasources: {
-    //         db: {
-    //         url: 'file:./dev_qa.db',
-    //         },
-    //     },
-    // }
-);
+export const prisma = new PrismaClient();
