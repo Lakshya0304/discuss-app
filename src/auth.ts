67d6d188fe6c6b4242e50656b02d522874f6eacd
Lakshya0304 <lakshyakhandelwal460@gmail.com>
2025-03-04 00:01:59 +0530

    setting database error

--- src/auth.ts
+++ src/auth.ts
@@ -4,11 +4,7 @@ import GitHubProvider from "next-auth/providers/github";
 import {prisma} from "@/lib/index"
 
 export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+  handlers: { GET, POST },auth,signIn,signOut,} = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
@@ -25,3 +21,25 @@ export const {
     },
   },
 });
+// export const {
+//   handlers: { GET, POST },
+//   auth,
+//   signIn,
+//   signOut,
+// } = NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ session, user }) {
+//       if (session && user) {
+//         session.user.id = user.id;
+//       }
+//       return session; // The return type will match the one returned in `useSession()`
+//     },
+//   },
+// });
